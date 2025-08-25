da9af7f4132b358c064ee4dcb9405497c510c818
Daniel Ruggeri <DRuggeri@primary.net>
2025-08-24 19:57:41 -0500

    Flesh out the counter client more

--- files/counterclient/counterclient.go
+++ files/counterclient/counterclient.go
@@ -4,26 +4,47 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
+	"github.com/CAFxX/balancer"
 	"github.com/gorilla/websocket"
 )
 
 var wsTarget = "ws://boss.local:8080/loadstats"
 var counterTarget = "http://counterserver.default.svc.cluster.local/hit"
+
+// Webslocket settings
 var reconnectDuration = 5 * time.Second
 var updateDuration = 100 * time.Millisecond
+
+// Client settings
+var numConcurrentClients = 20
+var numRequestsPerClient = 1000
 
 func main() {
 	mux := &sync.Mutex{}
 	okCounter := 0
 	nokCounter := 0
 	log := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
 	if tmp, ok := os.LookupEnv("WS_TARGET"); ok {
 		wsTarget = tmp
+	}
+
+	if tmp, ok := os.LookupEnv("NUM_CONCURRENT_CLIENTS"); ok {
+		if val, err := strconv.Atoi(tmp); err == nil && val > 0 {
+			numConcurrentClients = val
+		}
+	}
+	if tmp, ok := os.LookupEnv("NUM_REQUESTS_PER_CLIENT"); ok {
+		if val, err := strconv.Atoi(tmp); err == nil && val > 0 {
+			numRequestsPerClient = val
+		}
 	}
 
 	podname, _ := os.Hostname()
@@ -63,30 +84,49 @@ func main() {
 		fmt.Fprintf(w, `{ "OK": "%d", "NOK": "%d"}`, okCounter, nokCounter)
 	})
 
-	go func(*int, *int) {
-		for {
-			resp, err := http.Get(counterTarget)
-			if err != nil {
-				log.Error("error calling counter target", "error", err, "target", counterTarget)
-				mux.Lock()
-				nokCounter++
-				mux.Unlock()
-			} else {
-				io.ReadAll(resp.Body)
-				resp.Body.Close()
-				if resp.StatusCode != http.StatusOK {
-					log.Error("non-200 response from counter target", "status", resp.StatusCode, "target", counterTarget)
-					mux.Lock()
-					nokCounter++
-					mux.Unlock()
-				} else {
-					mux.Lock()
-					okCounter++
-					mux.Unlock()
+	for range numConcurrentClients {
+		go func(*int, *int) {
+			for {
+				resolver := &balancer.CachingResolver{
+					Resolver: &balancer.SingleflightResolver{
+						Resolver: &balancer.TimeoutResolver{
+							Resolver: net.DefaultResolver,
+							Timeout:  2 * time.Second,
+						},
+					},
+					TTL:    1 * time.Second,
+					NegTTL: 250 * time.Millisecond,
+				}
+				client := &http.Client{
+					Timeout: 1 * time.Second,
+				}
+				client.Transport = balancer.Wrap(http.DefaultTransport, resolver, "ip4")
+
+				for range numRequestsPerClient {
+					resp, err := client.Get(counterTarget)
+					if err != nil {
+						log.Error("error calling counter target", "error", err, "target", counterTarget)
+						mux.Lock()
+						nokCounter++
+						mux.Unlock()
+					} else {
+						io.Copy(io.Discard, resp.Body)
+						resp.Body.Close()
+						if resp.StatusCode != http.StatusOK {
+							log.Error("non-200 response from counter target", "status", resp.StatusCode, "target", counterTarget)
+							mux.Lock()
+							nokCounter++
+							mux.Unlock()
+						} else {
+							mux.Lock()
+							okCounter++
+							mux.Unlock()
+						}
+					}
 				}
 			}
-		}
-	}(&okCounter, &nokCounter)
+		}(&okCounter, &nokCounter)
+	}
 
 	slog.Info("Starting counter client", "port", "8080", "target", wsTarget, "service", counterTarget)
 	err := http.ListenAndServe(":8080", nil)
--- files/counterclient/go.mod
+++ files/counterclient/go.mod
@@ -2,4 +2,12 @@ module github.com/DRuggeri/counterclient
 
 go 1.24.0
 
-require github.com/gorilla/websocket v1.5.3
+require (
+	github.com/CAFxX/balancer v0.3.2
+	github.com/gorilla/websocket v1.5.3
+)
+
+require (
+	github.com/CAFxX/fastrand v0.1.0 // indirect
+	resenje.org/singleflight v0.3.0 // indirect
+)
